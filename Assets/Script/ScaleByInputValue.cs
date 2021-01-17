ef230de678ab178f77811cac49a431fddfe79a35
Daniel Lindmark <daniel.lindmark@madeintandem.com>
2021-01-17 16:52:54 -0600

    Renamed ScaleXYZCallback() Added ScaleXCallback()

--- Assets/Script/ScaleByInputValue.cs
+++ Assets/Script/ScaleByInputValue.cs
@@ -3,21 +3,57 @@ using UnityEngine.InputSystem;
 
 sealed class ScaleByInputValue : MonoBehaviour
 {
-    [SerializeField] Transform _transform = null;
-    [SerializeField] InputAction _action = null;
+    public float _maxScale = 1.0f;
+    public float _minScale = 0.1f;
+    [SerializeField] Transform _transform  = null;
+    [SerializeField] InputAction _ScaleXYZ = null;
+    [SerializeField] InputAction _ScaleX   = null;
+
 
     void OnEnable()
     {
-        _action.performed += OnPerformed;
-        _action.Enable();
+        _ScaleXYZ.performed += ScaleXYZCallback;
+        _ScaleXYZ.Enable();
+
+        _ScaleX.performed += ScaleXCallback;
+        _ScaleX.Enable();
     }
 
     void OnDisable()
     {
-        _action.performed -= OnPerformed;
-        _action.Disable();
+        _ScaleXYZ.performed -= ScaleXYZCallback;
+        _ScaleXYZ.Disable();
+
+        _ScaleXYZ.performed -= ScaleXCallback;
+        _ScaleX.Disable();
     }
 
-    void OnPerformed(InputAction.CallbackContext ctx)
-      => _transform.localScale = Vector3.one * ctx.ReadValue<float>();
+    void ScaleXYZCallback(InputAction.CallbackContext ctx)
+    {
+        float fScaleVal = ctx.ReadValue<float>() * _maxScale; 
+        //Vector3 vLocalScale = _transform.localScale;     
+
+        Vector3 vLocalScale =  _transform.localScale;
+        //vLocalScale.x = fScaleVal;
+        vLocalScale.y = fScaleVal;
+        vLocalScale.z = fScaleVal; 
+     
+        // Scale = Action * self * Max
+        _transform.localScale = vLocalScale;
+
+    }
+
+    void ScaleXCallback(InputAction.CallbackContext ctx)
+    {
+        //Vector3 vLocalScale =  Vector3.one;
+        Vector3 vLocalScale =  _transform.localScale;
+        vLocalScale.x = ctx.ReadValue<float>() * _maxScale;
+
+        // Scale = Action * x * Max
+        _transform.localScale = vLocalScale;
+    }
 }
+
+        // Vector3 vScale = _transform.localScale;
+        // vScale.y = ctx.ReadValue<float>(); // TODO add scaling
+        // _transform.localScale =  vScale;
